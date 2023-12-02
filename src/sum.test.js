107417906a6e809f48ef7188728a0c39b3d6b50c
eskokado <eskokado@gmail.com>
2023-12-02 12:08:57 -0300

    ci: fix sum

--- src/sum.test.js
+++ src/sum.test.js
@@ -4,6 +4,8 @@ test('add 1 + 2 to be equal 3', () => {
   expect(sum(1,2)).toBe(3);
 });
 
-test('add 1 + 2 to not equal 3', () => { 
+
+test('sum 1 + 2 to not equal 3', () => { 
   expect(sum(1,3)).not.toBe(3);
 });
+
