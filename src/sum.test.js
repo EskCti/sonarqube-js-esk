3d0e53f1f0641f0f4968972819f2d883949df019
eskokado <eskokado@gmail.com>
2023-12-02 12:04:56 -0300

    ci: sum js

---
+++ src/sum.test.js
@@ -0,0 +1,9 @@
+const sum = require("./sum");
+
+test('add 1 + 2 to be equal 3', () => { 
+  expect(sum(1,2)).toBe(3);
+});
+
+test('add 1 + 2 to not equal 3', () => { 
+  expect(sum(1,3)).not.toBe(3);
+});
