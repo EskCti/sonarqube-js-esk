7626766fe459b2c172c9685b40a1bf3b2986d8b7
eskokado <eskokado@gmail.com>
2023-12-06 09:22:57 -0300

    add multiply function

--- src/calcule.test.js
+++ src/calcule.test.js
@@ -1,5 +1,6 @@
 const sum = require("./sum");
 const subtract = require("./subtract");
+const multiply = require("./multiply");
 
 test('add 1 + 2 to be equal 3', () => { 
   
@@ -25,5 +26,19 @@ test('subtract 5 + 2 to be equal 3', () => {
 test('subtract 5 + 3 to not equal 3', () => { 
 
   expect(subtract(1,3)).not.toBe(3);
+
+});
+
+
+test('multiply 3 * 3 to be equal 9', () => { 
+  
+  expect(multiply(3,3)).toBe(9);
+
+});
+
+
+test('multiply 1 * 3 to not equal 4', () => { 
+
+  expect(multiply(1,3)).not.toBe(4);
 
 });
